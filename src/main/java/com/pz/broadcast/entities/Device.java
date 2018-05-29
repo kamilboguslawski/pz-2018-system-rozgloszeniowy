88a26179b66389d1166066f291e890275bd550fd
Bartłomiej Mroziński <bmrozinski7@gmail.com>
2018-05-29 23:42:28 +0200

    Adding lombok @Data annotation for creating setter, getter, equals, hash automatically.

--- src/main/java/com/pz/broadcast/entities/Device.java
+++ src/main/java/com/pz/broadcast/entities/Device.java
@@ -1,7 +1,10 @@
 package com.pz.broadcast.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Device {
 
@@ -22,53 +25,6 @@ public class Device {
         this.name = name;
         this.pushToken = pushToken;
         this.IPAddress = IPAddress;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPushToken() {
-        return pushToken;
-    }
-
-    public void setPushToken(String pushToken) {
-        this.pushToken = pushToken;
-    }
-
-    public String getIPAddress() {
-        return IPAddress;
-    }
-
-    public void setIPAddress(String IPAddress) {
-        this.IPAddress = IPAddress;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    @Override
-    public String toString() {
-        return "Device{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", pushToken='" + pushToken + '\'' +
-                ", IPAddress='" + IPAddress + '\'' +
-                '}';
     }
 
 }
--- src/main/java/com/pz/broadcast/entities/UserGroup.java
+++ src/main/java/com/pz/broadcast/entities/UserGroup.java
@@ -1,8 +1,11 @@
 package com.pz.broadcast.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.List;
 
+@Data
 @Entity
 public class UserGroup {
 
@@ -22,52 +25,6 @@ public class UserGroup {
     public UserGroup(String name, String description) {
         this.name = name;
         this.description = description;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
-    public List<File> getFiles() {
-        return files;
-    }
-
-    public void setFiles(List<File> files) {
-        this.files = files;
-    }
-
-    @Override
-    public String toString() {
-        return "UserGroup{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
     }
 
 }
