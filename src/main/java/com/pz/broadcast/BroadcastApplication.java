b95e83c1e93dd38aa7062a7f521d05941c5f82d0
DevNon <dawid.krysinski111@gmail.com>
2018-05-30 14:48:23 +0200

    Create system for storing files on server.

--- src/main/java/com/pz/broadcast/BroadcastApplication.java
+++ src/main/java/com/pz/broadcast/BroadcastApplication.java
@@ -1,7 +1,10 @@
 package com.pz.broadcast;
 
+import com.pz.broadcast.services.file.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
@@ -16,7 +19,13 @@ import org.springframework.context.annotation.ImportResource;
         "com.pz.broadcast.utils"
 })
 public class BroadcastApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(BroadcastApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroadcastApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return args -> storageService.init();
+	}
 }
---
+++ src/main/java/com/pz/broadcast/controllers/FileController.java
@@ -0,0 +1,32 @@
+package com.pz.broadcast.controllers;
+
+import com.pz.broadcast.services.file.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class FileController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+
+    @PostMapping("/uploadFile")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute(
+                "message",
+                String.format("File - %s - has been successfully uploaded.", file.getOriginalFilename())
+        );
+
+        return "";
+    }
+}
